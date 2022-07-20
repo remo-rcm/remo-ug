56609e9c6243480b3f4acc6040285fedad7dccb3
Lars Buntemeyer <5659125+larsbuntemeyer@users.noreply.github.com>
2022-07-20 21:27:10 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,8 +29,7 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  "myst_parser",
-  "recommonmark"
+  "myst_parser"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
